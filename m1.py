0f681101e15b902df6090689d50b6c997efd1394
Richard2111 <richardandrade2111@gmail.com>
2024-02-14 15:52:50 -0400

    actualizacion

--- m1.py
+++ m1.py
@@ -1,20 +1,34 @@
+import pymysql
+
+'''
+conexion = pymysql.connect(
+    host="localhost",
+    user="usuario",
+    password="contraseña",
+    database="nombre_de_la_base_de_datos",
+)
+cursor = conexion.cursor()
+'''
+
 def mostrar_menu():
     print("------------------")
     print("  Menú Principal  ")
     print("------------------")
     print("(1). Registro Propietario")
-    print("(2). Abono Propietario")
-    print("(3). Agregar Cuota especial")
-    print("(4). Salir")
+    print("(2). Consulta Deuda Propietario")
+    print("(3). Abono Propietario")
+    print("(4). Agregar Cuota especial")
+    print("(5). Salir")
     print("------------------")
+
 
 while True:
     mostrar_menu()
     opcion = int(input("Digite una opción: "))
 
     if opcion == 1:
-        print("Creando el Registro del Propietario ...")
         # Implementar la funcionalidad de la opción 1
+        print("Creando el Registro del Propietario ...")
         nombre = str(input("Escriba el nombre del propietario: "))
         apellido = str(input("Escriba el Apellido del propietario: "))
         numero_de_casa = int(input("Escriba el numero de casa del propietario: "))
@@ -29,13 +43,41 @@ while True:
         print(deuda_acumulada_bolivares)
         print(deuda_acumulada_dolares)
     elif opcion == 2:
-        print("Creando el abono del propietario ...")
-        # Implementar la funcionalidad de la opción 2
+        print("------------------")
+        print("  Menú Principal  ")
+        print("------------------")
+        print("(1). Consulta deuda del propietario")
+        print("(2). Consulta Deuda general")
+        print("(3). Salir")
+        print("------------------")
+
+        consultar_deuda_opcion = int(input("Digite una opción: "))
+
+        while True:
+
+                if consultar_deuda_opcion == 1:
+                    print("Consulta de deuda del propietario ...")
+                    numero_de_casa = int(input("Escriba el numero de casa del propietario: "))
+                    break
+                elif consultar_deuda_opcion == 2:
+                    print("Consulta de deuda general de los propietarios ...")
+                    break
+
+                elif consultar_deuda_opcion == 3:
+                    print("saliendo del submenu")
+                    break
+                else:
+                    print("opcion no valida.")
+                    break
+
     elif opcion == 3:
-        print("Agregando Cuota Espcecial1")
         # Implementar la funcionalidad de la opción 3
+        print("Creando el abono del propietario ...")
     elif opcion == 4:
-        print("Saliendo del programa...")
+        # Implementar la funcionalidad de la opción 4
+        print("Agregando Cuota Espcecial1")
+    elif opcion == 5:
+        print("Saliendo del programa..5.")
         break
     else:
         print("Opción no válida.")
